804e1ad6784a7c80f21521bac7da87cec1a1609f
Antua <suleykin2005@mail.ru>
2026-03-10 21:54:08 +0300

    first prototype

--- app/src/main/java/com/example/a412/MainActivity.kt
+++ app/src/main/java/com/example/a412/MainActivity.kt
@@ -4,14 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.a412.ui.theme._412Theme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,8 +43,7 @@ class MainActivity : ComponentActivity() {
             _412Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding), viewModel = viewModelTwelve()
                     )
                 }
             }
@@ -31,17 +52,57 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Greeting(modifier: Modifier = Modifier, viewModel: viewModelTwelve) {
+    var fact by remember { mutableStateOf<String?>(null) }
+    var loading by remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text("Факты о животных!", fontSize = 40.sp, fontWeight = Bold)
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(
+            onClick = {
+                scope.launch {
+                    loading = true
+                    fact = null
+                    viewModel.getRandomFact().collect {
+                        fact = it
+                        loading = false
+                    }
+                }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    _412Theme {
-        Greeting("Android")
+            }
+        ) {
+            Text("Новый факт!", fontSize = 18.sp)
+        }
+        Spacer(modifier = Modifier.height(30.dp))
+        if (loading) {
+            CircularProgressIndicator()
+        }
+        // клёво
+        fact?.let {
+            Card(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .animateContentSize(),
+            ) {
+                Text(
+                    text = it,
+                    modifier = Modifier.padding(16.dp),
+                    fontSize = 18.sp
+                )
+            }
+        }
     }
 }
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    _412Theme {
+//        Greeting("Android")
+//    }
+//}
